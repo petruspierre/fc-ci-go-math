73b1f2650af0b8d0fd24c743c521e90c59769921
Petrus Pierre <petrus.tsx@gmail.com>
2022-09-08 16:16:19 -0300

    Revert "Feature/sonar qube"

--- math.go
+++ math.go
@@ -3,21 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(Sum(112, 10))
+	fmt.Println(Soma(112, 10))
 }
 
-func Sum(a int, b int) int {
+func Soma(a int, b int) int {
 	return a + b
-}
-
-func Sub(a int, b int) int {
-	return a - b
-}
-
-func Times(a int, b int) int {
-	return a * b
-}
-
-func SumX(a int, b int) int {
-	return a + b + a
 }
--- math_test.go
+++ math_test.go
@@ -2,8 +2,8 @@ package main
 
 import "testing"
 
-func TestSum(t *testing.T) {
-	total := Sum(15, 15)
+func TestSoma(t *testing.T) {
+	total := Soma(15, 15)
 
 	if total != 30 {
 		t.Errorf("Resultado da some é inválido: Resultado %d. Esperado: %d", total, 30)
